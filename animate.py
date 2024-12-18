19b8e6c97ad922c6208fab48a1a1ebe65644845d
Jonathan Chen <126440908+tindicatrix@users.noreply.github.com>
2024-12-18 12:39:31 -0500

    Update animate.py

--- animate.py
+++ animate.py
@@ -3,15 +3,14 @@ import numpy as np
 import pandas as pd
 import os
 
-saveDirectory = 'raymond'
+saveDirectory = 'Raymond' #can be whatever you want, your name probably
 #create save directory
 if not os.path.exists(saveDirectory): #make folder if doesn't exist
     os.makedirs(saveDirectory)
   
 #csv source url
-#url = 'https://drive.google.com/uc?export=download&id=1LqAk1xIyuNq0ynJK8mkuvBTgxXCOK2SH' #big circle
+#direct link from https://sites.google.com/site/gdocs2direct/, csv file
 url = 'https://drive.google.com/uc?export=download&id=1ith3awBE8vTcpVGo6o3SVQj2aZhW5f4o' #raymond's
-#url = 'https://drive.google.com/uc?export=download&id=1M6I4BP4ZFkuD_gF8XBmljCutsn2JTwng' #squircle star
 
 try:
   reader = pd.read_csv(url)
@@ -36,14 +35,12 @@ def sinDFT(y,t,n):
   comparisonValues = np.sin(n*t*2*np.pi/L)
   dotprod = np.dot(y,comparisonValues)
   norm = np.dot(comparisonValues,comparisonValues)
-
   return dotprod/norm
 
 def cosDFT(y,t,n):
   comparisonValues = np.cos(n*t*2*np.pi/L)
   dotprod = np.dot(y,comparisonValues)
   norm = np.dot(comparisonValues,comparisonValues)
-
   return dotprod/norm
 
 def constant(y,samples):
@@ -54,7 +51,6 @@ def DFT(y,t):
   for n in range(1,int((len(t)+1)/2)):
     sinls.append(sinDFT(y,t,n))
     cosls.append(cosDFT(y,t,n))
-  
   return (sinls,cosls)
 
 def fourier(t,sinterms,costerms,y,numterms):
@@ -62,10 +58,9 @@ def fourier(t,sinterms,costerms,y,numterms):
   for i in range(numterms):
     total += sinterms[i]*np.sin((i+1)*t*(2*np.pi/L)) + costerms[i]*np.cos((i+1)*t*(2*np.pi/L))
   total += constant(y,samples)
-
   return total
 
-
+#compute the values using DFT
 ySins = DFT(yPoints,t)[0]
 yCoses = DFT(yPoints,t)[1]
 xSins = DFT(xPoints,t)[0]
@@ -73,6 +68,7 @@ xCoses = DFT(xPoints,t)[1]
 
 tVals = np.linspace(1,samples,10000)
 
+#axes
 xmin = np.min(xPoints)-np.mean(xPoints)/10
 xmax = np.max(xPoints)+np.mean(xPoints)/10
 ymin = np.min(yPoints)-np.mean(yPoints)/10
@@ -84,7 +80,7 @@ def makeImg(numterms):
   plt.axis([xmin,xmax,ymin,ymax])
   plt.title(f'number of terms = {numterms}')
   plt.plot(fourier(tVals,xSins,xCoses,xPoints,numterms),fourier(tVals,ySins,yCoses,yPoints,numterms))
-  plt.savefig(saveDirectory+'/'+f"{numterms:03d}") #need to change the f string to match the number of digits in the num of points there are
+  plt.savefig(saveDirectory+'/'+f"{numterms:04d}") #need to change the f string to match the number of digits in the num of points there are
   plt.close()
 
 #frames
@@ -101,18 +97,15 @@ from PIL import Image
 
 # filepaths
 fp_in = saveDirectory+"/*.png"
-fp_out = saveDirectory+".gif"
+fp_out = saveDirectory+"/!"+saveDirectory+".gif"
 
 # use exit stack to automatically close opened images
 with contextlib.ExitStack() as stack:
-
     # lazily load images
     imgs = (stack.enter_context(Image.open(f))
             for f in sorted(glob.glob(fp_in)))
-
     # extract  first image from iterator
     img = next(imgs)
-
     # https://pillow.readthedocs.io/en/stable/handbook/image-file-formats.html#gif
     img.save(fp=fp_out, format='GIF', append_images=imgs,
              save_all=True, duration=200, loop=0)
